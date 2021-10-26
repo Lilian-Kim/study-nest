a49a67b1d87099dd82a1f19a2f9bbd49980bedcd
Lilian-Kim <lilian.k3104@gmail.com>
2021-10-26 18:57:26 +0900

    try catch 에러처리

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,24 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException('이미 존재하는 이름입니다.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
