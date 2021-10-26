547838368a34ac93de14f4d0307739ec8554acd3
Lilian-Kim <lilian.k3104@gmail.com>
2021-10-26 18:50:22 +0900

    유효성 체크 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
 export class AuthCredentialDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: '비밀번호는 영어와 숫자만 가능합니다.',
+  })
   password: string;
 }
