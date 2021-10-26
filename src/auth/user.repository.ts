2b07b0b13f8ffcb5a27339d141ff5b8c62d5e262
Lilian-Kim <lilian.k3104@gmail.com>
2021-10-26 19:10:07 +0900

    오타 수정

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -11,7 +11,7 @@ import {
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const salt = await bcrypt.getSalt();
+    const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
     const user = this.create({ username, password: hashedPassword });
     try {
